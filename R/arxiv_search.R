e8607ff601e0b436e3767cc18231ea562183bc9c
Karthik Ram <karthik.ram@gmail.com>
2014-03-03 20:13:06 -0800

    Current status before pushing to GitHub

--- R/arxiv_search.R
+++ R/arxiv_search.R
@@ -11,15 +11,25 @@
 #' @param  start An offset for the start of search
 #' @param  end Where to end search results
 #' @param  ... optional additional arguments
+#' @importFrom XML xmlToList
+#' @importFrom rjson toJSON
+#' @import httr
 #' @export
 #' @keywords
 #' @seealso
 #' @return
 #' @alias
 #' @examples \dontrun{
-#'
+#' arxiv_search(query = "electron")
 #'}
-arxiv_search  <- function(terms = NA, start = NA, end = NA, ...) {
-	# Throwing down a dummy function to generate a skeleton.
-	invisible()
+arxiv_search  <- function(query = NULL, start = NA, end = NA, foptions = list()) {
+	browser()
+ args <- compact(as.list(c(query = query)))	
+ query_url <- "http://export.arxiv.org/api/query"
+ search_res <- GET(query_url, query = args, foptions)
+ stop_for_status(search_res)
+ search_content <- content(search_res)
+ search_list <- xmlToList(search_content)
+ search_json <- toJSON(search_list)
+ search_json
 }
