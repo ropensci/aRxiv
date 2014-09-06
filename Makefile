98440c0f189960f6d2d8cebb6135f7f9063db68d
Karl Broman <kbroman@gmail.com>
2014-09-06 08:55:22 -0500

    Initial stab at arxiv_search; just returns the xml as a list

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
+all: doc notes
+
+notes: inst/doc/arxiv_api.html
+
 inst/doc/arxiv_api.html: inst/doc/src/arxiv_api.md
 	R -e 'library(markdown);markdownToHTML("$<", "$@")'
+
+doc:
+	R -e 'library(devtools);document()'
--- R/arxiv_search.R
+++ R/arxiv_search.R
@@ -7,29 +7,41 @@
 #'The main search function for aRxiv
 #'
 #'Allows for progammatic searching of the arXiv pre-print repository.
-#' @param terms  Search terms. Submit multiple terms as a concactenated string.
+#' @param query  Search pattern; submit multiple terms as a concactenated string.
+#' @param id_list List of arXiv doc IDs, as comma-delimited string
 #' @param  start An offset for the start of search
 #' @param  end Where to end search results
-#' @param  ... optional additional arguments
-#' @importFrom XML xmlToList
-#' @importFrom rjson toJSON
+#' @param sort_by How to sort the results
+#' @param ascending If TRUE, sort in increasing order; else decreasing
+#' @importFrom XML xmlToList getNodeSet xmlParse
 #' @import httr
 #' @export
-#' @keywords
-#' @seealso
-#' @return
-#' @alias
-#' @examples \dontrun{
-#' arxiv_search(query = "electron")
-#'}
-arxiv_search  <- function(query = NULL, start = NA, end = NA, foptions = list()) {
-	browser()
- args <- compact(as.list(c(query = query)))	
- query_url <- "http://export.arxiv.org/api/query"
- search_res <- GET(query_url, query = args, foptions)
- stop_for_status(search_res)
- search_content <- content(search_res)
- search_list <- xmlToList(search_content)
- search_json <- toJSON(search_list)
- search_json
+#' @return Parse XML result as a list
+#' @examples
+#' z <- arxiv_search(query = "au:Broman AND cat:stat.AP", start=0, end=10)
+#' z$totalResults
+#' titles <- sapply(z[names(z)=="entry"], function(a) a$title)
+arxiv_search <-
+function(query = NULL, id_list=NULL, start = NULL, end = NULL,
+         sort_by=c("relevance", "lastUpdatedDate", "submittedDate"),
+         ascending=TRUE)
+{
+    query_url <- "http://export.arxiv.org/api/query"
+
+    sort_by <- match.arg(sort_by)
+    sort_order <- ifelse(ascending, "ascending", "descending")
+
+    search_res <- POST(query_url,
+                       body=list(search_query=query, id_list=id_list,
+                                 start=start, max_results=end-start+1,
+                                 sort_by=sort_by, sort_order=sort_order))
+
+    stop_for_status(search_res)
+
+    doc <- xmlParse(content(search_res, "text"), asText=TRUE)
+    nodes <- rapply(list(doc), function(a) getNodeSet(a, path="/"),
+                    how="replace")
+    result <- rapply(nodes, function(x) xmlToList(x), how="replace")[[1]][[1]][[1]]
+
+    result
 }
