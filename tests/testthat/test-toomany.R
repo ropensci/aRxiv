cb743cf4d56ea8c90a351f4d42688d0a04541ab0
Karl Broman <kbroman@gmail.com>
2014-09-06 14:06:57 -0500

    fix tests of is_too_many
     - results changed since defaults for start and end changed

--- tests/testthat/test-toomany.R
+++ tests/testthat/test-toomany.R
@@ -4,16 +4,16 @@ context("is_too_many")
 test_that("is_too_many gives reasonable info", {
 
     # this search should give a very large number
-    expect_true(is_too_many("au:A") > 170000)
+    expect_true(is_too_many("au:A", start=0, end=NULL) > 170000)
 
     # this search should give 0 (not too many results)
-    expect_equal(is_too_many("au:Ernie AND au:Bert AND au:WalterWhite"), 0)
+    expect_equal(is_too_many("au:A", start=0, end=10), 0)
 
 })
 
 test_that("arxiv_search throws error with huge result", {
 
     # should give error, to prevent huge result
-    expect_error(arxiv_search("au:A"))
+    expect_error(arxiv_search("au:A", end=NULL))
 
 })
