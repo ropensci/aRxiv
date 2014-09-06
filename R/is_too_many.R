fa973400143ea5fa4b2906c6b4d83e0c718106ad
Karl Broman <kbroman@gmail.com>
2014-09-06 14:05:48 -0500

    is_too_many: revise defaults for start and end

--- R/is_too_many.R
+++ R/is_too_many.R
@@ -1,11 +1,22 @@
 # check if a search will return too many results
 is_too_many <-
-function(query=NULL, id_list=NULL, start=NULL, end=NULL)
+function(query=NULL, id_list=NULL, start=0, end=10)
 {
     toomany <- 15000 # I'll call this too many
 
+    expected_count <- NA
+
+    if(is.null(start))
+        start <- 0
+    if(is.null(end))
+        end <- expected_number <- arxiv_count(query, id_list)
+
+    stopifnot(start >= 0)
+    stopifnot(end >= 0)
+
     if(end-start+1 > toomany) {
-        expected_number <- arxiv_count(query, id_list)
+        if(is.na(expected_number))
+            expected_number <- arxiv_count(query, id_list)
         message("Total records matching query: ", expected_number)
 
         if(expected_number > toomany)
