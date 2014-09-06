6e827943ccf3ef1a8b65d6e7eb59d9963a34bed9
Karl Broman <kbroman@gmail.com>
2014-09-06 13:55:37 -0500

    Code to space out arxiv search requests
     - before call to POST, call delay_if_necessary()
     - sets environment variable aRxiv_time

---
+++ R/time.R
@@ -0,0 +1,34 @@
+# make sure not to make calls too often
+
+# get time of last call
+get_arxiv_time <-
+function()
+{
+   last <- as.numeric(Sys.getenv("aRxiv_time"))
+   ifelse(last=="", 0, last)
+}
+
+# set arxiv time to current
+set_arxiv_time <-
+function() {
+    Sys.setenv(aRxiv_time = as.numeric(Sys.time()))
+}
+
+# time since last call
+time_since_arxiv <-
+function() {
+    as.numeric(Sys.time()) - get_arxiv_time()
+}
+
+# check for last time since call, and delay if necessary
+# also re-set the arxiv_time
+delay_if_necessary <-
+function()
+{
+    delay_amount <- 3
+
+    if((timesince = time_since_arxiv()) < delay_amount)
+        Sys.sleep(delay_amount - timesince)
+
+    set_arxiv_time()
+}
