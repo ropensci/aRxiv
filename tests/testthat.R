1977ea37be4ccaa8c2eb89350b356c20317dc8a0
Karl Broman <kbroman@gmail.com>
2014-12-16 14:59:37 -0600

    Function to test connection to arXiv; skip tests if can't connect

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,2 +1,10 @@
 library(testthat)
-test_check("aRxiv")
+library(aRxiv)
+if(can_arxiv_connect()) {
+    # run only if can connect to aRxiv
+    test_check("aRxiv")
+} else {
+    # if can't connect and not CRAN, throw an error
+    if (identical(Sys.getenv("NOT_CRAN"), "true"))
+        error("Can't connect to aRxiv")
+}
