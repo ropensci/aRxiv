978d689a7bd7bb85934e04508dab0b9f7c30f73f
Karl Broman <kbroman@gmail.com>
2014-12-16 17:35:49 -0600

    testthat.R: aRxiv -> arXiv

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,11 @@
 library(testthat)
 library(aRxiv)
+
 if(can_arxiv_connect()) {
-    # run only if can connect to aRxiv
+    # run only if can connect to arXiv
     test_check("aRxiv")
 } else {
     # if can't connect and not CRAN, throw an error
     if (identical(Sys.getenv("NOT_CRAN"), "true"))
-        error("Can't connect to aRxiv")
+        error("Can't connect to arXiv")
 }
