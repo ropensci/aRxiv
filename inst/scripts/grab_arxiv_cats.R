a7fd0ac84210295b0279d308a79a2d340a93dbe8
Karl Broman <kbroman@gmail.com>
2024-01-22 08:11:46 -0600

    Finish script to grab arxiv categories

--- inst/scripts/grab_arxiv_cats.R
+++ inst/scripts/grab_arxiv_cats.R
@@ -14,13 +14,12 @@ library(rvest)
 url <- "https://arxiv.org/category_taxonomy"
 text <- content(GET(url), encoding="UTF-8")
 
+# category ID
 ids <- html_elements(text, "div h4")[-1] # first one is not a real category
 ids <- sapply(strsplit(as.character(ids), "<h4>"), "[", 2)
 ids <- sapply(strsplit(as.character(ids), " <span>"), "[", 1)
 
-# categories
-cats <- sapply(strsplit(ids, ".", fixed=TRUE), "[", 1)
-
+# short description
 short_descr <- html_elements(text, "div h4 span")
 short_descr <- sub("<span>(", "", as.character(short_descr), fixed=TRUE)
 short_descr <- sub(")</span>", "", as.character(short_descr), fixed=TRUE)
@@ -28,11 +27,13 @@ short_descr <- sub(")</span>", "", as.character(short_descr), fixed=TRUE)
 # should be the same length
 stopifnot(length(ids) == length(short_descr))
 
-larger_cat <- html_elements(text, "h2.accordion-head")
-larger_cat <- sapply(strsplit(as.character(larger_cat), "[<>]"), "[", 3)
+# highest-level fields
+fields <- html_elements(text, 'h2.accordion-head')
+fields <- sapply(strsplit(as.character(fields), "[<>]"), "[", 3)
 
-cat_abbr <- sapply(strsplit(ids, ".", fixed=TRUE), "[", 1)
-cat_abbr_uniq <- unique(larger_cat_abbr)
+# field abbreviation
+fields_abbr <- sapply(strsplit(ids, ".", fixed=TRUE), "[", 1)
+fields_abbr_uniq <- unique(fields_abbr)
 
 # ugh there are two levels of categories to extract, as well as short and long descriptions
 long_descr <- html_elements(text, "div.columns p")
@@ -40,9 +41,6 @@ long_descr <- sapply(strsplit(as.character(long_descr), "[<>]"), "[", 3)
 long_descr <- long_descr[3:(length(long_descr)-1)] # drop first two plus last
 stopifnot(length(long_descr) == length(short_descr))
 
-# highest-level fields
-fields <- html_elements(text, 'h2.accordion-head')
-fields <- sapply(strsplit(as.character(fields), "[<>]"), "[", 3)
 
 # just physics is split up into sub-fields; look for these sub-fields
 sub_fields_physics <- html_elements(text, 'div.physics div h3')
@@ -51,21 +49,26 @@ sub_fields_physics <- sapply(strsplit(as.character(sub_fields_physics), "[<>]"),
 sub_fields_physics_abbr <- gsub("[\\(\\)]", "", sub_fields_physics_abbr)
 stopifnot(length(sub_fields_physics) == length(sub_fields_physics_abbr))
 
-# expand fields and sub-fields
-physics_cats <- cats %in% sub_fields_physics_abbr
-fields_abbr <- ()
+# create fields and subfields objects of same length as ids
+fields_uniq <- fields_abbr_uniq
+fields_uniq[fields_abbr_uniq %in% sub_fields_physics_abbr] <- "Physics"
+fields_uniq[!(fields_abbr_uniq %in% sub_fields_physics_abbr)] <- fields[fields != "Physics"]
+fields_count <- sapply(fields_abbr_uniq, function(a) sum(fields_abbr ==a))
+fields <- rep(fields_uniq, fields_count)
 
+sub_fields_physics_count <- sapply(sub_fields_physics_abbr, function(a) sum(fields_abbr == a))
+sub_fields_physics <- rep(sub_fields_physics, sub_fields_physics_count)
+subfields <- fields
+subfields[fields == "Physics"] <- sub_fields_physics
+subfields[fields != "Physics"] <- NA
 
-arxiv_cats <- data.frame(abbreviation=ids,
-                         field=,
-                         subfield=,
+arxiv_cats <- data.frame(category=ids,
+                         field=fields,
+                         subfield=subfields,
                          short_description=short_descr,
                          long_description=long_descr)
 
-colnames(arxiv_cats) <- c("abbreviation", "description")
-arxiv_cats <- arxiv_cats[-1,] # drop header row
 rownames(arxiv_cats) <- 1:nrow(arxiv_cats)
 
 ## save as data sets within package
-save(query_terms, file="../../data/query_terms.RData")
 save(arxiv_cats, file="../../data/arxiv_cats.RData")
